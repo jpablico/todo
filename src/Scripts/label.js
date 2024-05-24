3d19f2c70b5e41d52a3eae20b28c52e71e6f01a6
Josh Pablico <jbpablico27@gmail.com>
2024-05-24 18:32:14 -0500

    JS

--- src/Scripts/label.js
+++ src/Scripts/label.js
@@ -3,3 +3,18 @@ const labelAddition = document.getElementsByClassName('labels-addition')[0];
 addEventListener('click', (e) => {
 	console.log("bop");
 });
+
+labelAddition.addEventListener('click', (e) => {
+	labelAddition.forEach(el => {
+		el.style.display = "none"
+	})
+};
+
+const toggleElements = document.querySelectorAll('.toggle');
+toggleElements.forEach(el => {
+	el.addEventListener('click', () => {
+		this.classList.toggle('active'); // `this` refers to `Window`
+		
+	});
+});
+
